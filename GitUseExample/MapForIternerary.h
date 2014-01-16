f3337415e9d92b51623ee8e446ab12ee2d13c9db
cnespinoza <casandraespinoza@Casandras-MacBook-Pro.local>
2014-01-16 10:57:30 -0800

    Adding a map

---
+++ GitUseExample/MapForIternerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForIternerary.h
+//  GitUseExample
+//
+//  Created by Casandra Espinoza on 1/16/14.
+//  Copyright (c) 2014 Casandra Espinoza. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForIternerary : NSObject
+
+@end
---
+++ GitUseExample/MapForIternerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForIternerary.m
+//  GitUseExample
+//
+//  Created by Casandra Espinoza on 1/16/14.
+//  Copyright (c) 2014 Casandra Espinoza. All rights reserved.
+//
+
+#import "MapForIternerary.h"
+
+@implementation MapForIternerary
+
+@end
