8daf049903337edd751a6b695e67e39d9b64a9cd
cnespinoza <casandraespinoza@Casandras-MacBook-Pro.local>
2014-01-16 10:31:57 -0800

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by Casandra Espinoza on 1/16/14.
+//  Copyright (c) 2014 Casandra Espinoza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUseExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitUseExample
+//
+//  Created by Casandra Espinoza on 1/16/14.
+//  Copyright (c) 2014 Casandra Espinoza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
